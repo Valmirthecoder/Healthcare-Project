4315f948947bdfdb3af0e97b93b7235868e2da32
Valmirthecoder <valmir.haxholli@student.cacttus.education>
2019-01-16 22:20:26 +0100

    made some changes in users login and signup

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,21 +18,35 @@
 			</a>
 			<ul>
 				<li><a href="index.php">Home</a></li>
-				<li><a href="">Portofolio</a></li>
-				<li><a href="">About</a></li>
-				<li><a href="">Contact</a></li>
 			</ul>
+			</nav>
+
 			<div>
-				<form action="includes/login.inc.php" method="post">
-					<input type="text" name="mailuid" placeholder="Username/Email...">
-					<input type="password" name="pwd" placeholder="Password...">
-					<button type="submit" name="login-submit">Login</button>
-				</form>
-				<a href="signup.php">Signup</a>
-				<form action="includes/logout.inc.php" method="post">
-					<button type="submit" name="logout-submit">Logout</button>
-				</form>
+
+
+				<?php
+
+					if (isset($_SESSION['userId'])) {
+						echo '<form action="includes/logout.inc.php" method="post">
+						<button type="submit" name="logout-submit">Logout</button>
+						</form>';
+					}
+					else  {
+						echo '<form action="includes/login.inc.php" method="post">
+						<input type="text" name="mailuid" placeholder="Username/Email...">
+						<input type="password" name="pwd" placeholder="Password...">
+						<button type="submit" name="login-submit">Login</button>
+						</form>
+						<a href="signup.php">Signup</a>';
+					}
+				?>
+
+
+
+
+				
+				
 			</div>
-		</nav>
+		
 	</header>
 
